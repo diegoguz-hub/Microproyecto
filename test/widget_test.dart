43e92e130507368850d42a446bd83f0da72fb3fb
danipacheco205 <danipacheco@correo.unimet.edu.ve>
2026-02-02 20:49:56 -0400

    Update widget_test.dart
    Updated the widget test to use MemoryGameApp and changed expectations accordingly.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,18 +13,13 @@ import 'package:microproyecto/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MemoryGameApp());
+    
+    expect(find.text('MetroMemory'), findsOneWidget);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Intentos: 0'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byIcon(Icons.help_outline), findsWidgets);
   });
 }
+
